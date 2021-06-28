f022172782e2748863e4e6940a90fdf5a7de488c
Eabdalhaleem <eattaamneh@yahoo.com>
2021-06-28 21:02:46 +0300

    lab06

--- src/App.js
+++ src/App.js
@@ -1,48 +1,75 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Card } from 'react-bootstrap';
 
 export class App extends Component {
 
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={
-      displayName:'',
-      latitude:'',
-      longitude:'',
+    this.state = {
+      displayName: '',
+      latitude: '',
+      longitude: '',
       show: false,
       error: ''
-
     }
 
   }
 
-  cityNameChange=(e)=>{
+  cityNameChange = (e) => {
     this.setState({
-      displayName:e.target.value
+      displayName: e.target.value
     })
 
 
   }
-  submitData=(e)=>{
-    let axiosResposne= axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.ea18e02e564491ab24720e046283f45f&city=Amman&format=json`)
+  submitData = async (e) => {
     e.preventDefault()
-    this.setState({
+    try {
+      let axiosResposne = await axios.get(`https://us1.locationiq.com/v1/search.php?key=pk.ea18e02e564491ab24720e046283f45f&city=${this.state.displayName}&format=json`)
 
-    })
+      this.setState({
+        displayName: axiosResposne.data[0].displayName,
+        latitude: axiosResposne.data[0].lat,
+        longitude: axiosResposne.data[0].lon,
+        show: !!!this.state.show,
+        error: ''
+      })
+    }
+    catch {
+      this.setState({
+        error: 'oops !!'
+      })
+    }
   }
-  render() {
-    return (
-      <div>
-        <form>
-          <input type='text' placeholder='city explorer' onChange={(e)=>{this.cityNameChange(e)}}/>
-          <button onClick>Explor</button>
-        </form>
-        <h1>{this.state.displayName}</h1>
-        <h3>{this.state.latitude}</h3>
-        <h3>{this.state.longitude}</h3>
-      </div>
-    )
-  }
+
 }
+
+render(){
+  return (
+    <div>
+      <form onSubmit={this.submitData}>
+        <input type='text' placeholder='city explorer' onChange={(e) => { this.cityNameChange(e) }} />
+        <button onClick>Explor city</button>
+      </form>
+      <h1>{this.state.displayName}</h1>
+      <h3>{this.state.latitude}</h3>
+      <h3>{this.state.longitude}</h3>
+
+      <Card className="text-center">
+        <Card.Header>city Explorere</Card.Header>
+        <Card.Body>
+          <Card.Title>{this.state.displayName}</Card.Title>
+          <Card.Text>
+          {this.state.latitude}
+          {this.state.longitude}
+          </Card.Text>
+          <Card.Img variant="top" src={`https://maps.locationiq.com/#zoom=10.44&lat=31.972154&lon=35.903662&style=Dark=${this.state.latitude},${this.state.longitude}&zoom=20&format=png`}width='400px' height='400px />
+        </Card.Body>
+      </Card>
+    </div>
+  )
+  }
+
 
 export default App
